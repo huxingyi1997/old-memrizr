f8790b5c48160a6e1dd70c00ac13efddcac09fb5
xingyi.hu <xingyi.hu@shopee.com>
2022-08-11 21:11:03 +0800

    01-2

--- account/main.go
+++ account/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,4 +35,28 @@ func main() {
 			log.Fatalf("Failed to initialize server: %v\n", err)
 		}
 	}()
+
+	log.Printf("Listening on port %v\n", srv.Addr)
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// This blocks until a signal is passed into the quit channel
+	<-quit
+
+	log.Println("Shutting down server...")
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
