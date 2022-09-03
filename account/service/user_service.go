51f59b1c59f5c19b76b7cea8a71157d932473596
xingyi.hu <xingyi.hu@shopee.com>
2022-09-03 12:03:58 +0800

    08

--- account/service/user_service.go
+++ account/service/user_service.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"log"
 
 	"github.com/google/uuid"
 	"github.com/huxingyi1997/memrizr/account/model"
+	"github.com/huxingyi1997/memrizr/account/model/apperrors"
 )
 
 // UserService acts as a struct for injecting an implementation of UserRepository
@@ -37,5 +39,20 @@ func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, erro
 // SignUp reaches our to a UserRepository to verify the
 // email address is available and signs up the user if this is the case
 func (s *UserService) Signup(ctx context.Context, u *model.User) error {
-	panic("Method not implemented")
+	pw, err := hashPassword(u.Password)
+
+	if err != nil {
+		log.Printf("Unable to signup user for email: %v\n", u.Email)
+		return apperrors.NewInternal()
+	}
+
+	// now I realize why I originally used Signup(ctx, email, password)
+	// then created a user. It's somewhat un-natural to mutate the user here
+	u.Password = pw
+
+	if err := s.UserRepository.Create(ctx, u); err != nil {
+		return err
+	}
+
+	return nil
 }
