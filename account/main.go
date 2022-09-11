bb0e597b70c847d23748b0ca4464e66a664ac203
xingyi.hu <xingyi.hu@shopee.com>
2022-09-11 16:31:47 +0800

    10

--- account/main.go
+++ account/main.go
@@ -8,27 +8,30 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/gin-gonic/gin"
-	"github.com/huxingyi1997/memrizr/account/handler"
 )
 
 func main() {
 	log.Println("Starting server...")
 
-	router := gin.Default()
-	handler.NewHandler(&handler.Config{
-		R: router,
-	})
+	// initialize data sources
+	ds, err := initDS()
+
+	if err != nil {
+		log.Fatalf("Unable to initialize data sources: %v\n", err)
+	}
+
+	router, err := inject(ds)
+
+	if err != nil {
+		log.Fatalf("Failure to inject data sources: %v\n", err)
+	}
 
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
-	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/notify-without-context/server.go
-	// Initializing the server in a goroutine so that
-	// it won't block the graceful shutdown handling below
+	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("Failed to initialize server: %v\n", err)
@@ -37,25 +40,27 @@ func main() {
 
 	log.Printf("Listening on port %v\n", srv.Addr)
 
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal, 1)
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
 
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
 	// This blocks until a signal is passed into the quit channel
 	<-quit
 
-	log.Println("Shutting down server...")
 	// The context is used to inform the server it has 5 seconds to finish
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
+
+	// shutdown data sources
+	if err := ds.close(); err != nil {
+		log.Fatalf("A problem occured gracefully shutting down data sources: %v\n", err)
 	}
 
-	log.Println("Server exiting")
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
 }
